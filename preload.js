ae491da1d4005e554d3a9bcf29ddfcf20e5f69d2
LafatraO1 <harilivaainahifaharana@gmail.com>
2025-10-01 09:24:19 +0300

    preload.js

--- preload.js
+++ preload.js
@@ -1,17 +1,5 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron")
 
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, process.versions[type]);
-  }
-});
-
-// Mamorona bridge azo ampiasaina any amin’ny renderer.js
 contextBridge.exposeInMainWorld("electronAPI", {
-  saveFile: (filePath, buffer) => ipcRenderer.invoke("save-file", filePath, buffer)
-});
+  getBooks: () => ipcRenderer.invoke("get-books")
+})
